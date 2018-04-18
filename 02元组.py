c345ae78c9c6e32ed35cf0bbc5c854a7984d9f98
KK_Swifter <2822072760@qq.com>
2018-04-18 17:12:58 +0800

    Add files via upload

---
+++ 02元组.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+#t = (1,[])
+##t[0] = 3
+#t[1].append(5)
+#print(t)
+
+#t = (1,2)
+#a,b = t
+#print(t)
+
+#import collections
+#
+#People = collections.namedtuple('People', ['name','age'])
+#p = People('bestswifter','28')
+#p.name
+#
+#print(p.name)
+
+#h = [1,2,3,4,5,6]
+##h[2:3] = ['0','8']
+#h[1:3] = ['0']
+#print(h)
+
