1f57b07bcb264ae695c765c62580846f9fd066d0
Mingriweiji-github <2822072760@qq.com>
2018-04-19 11:19:23 +0800

    04 changed

--- 04字典.py
+++ 04字典.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 #import requests
 #response = requests.get('https://httpbin.org/ip')
 #print('Your IP is {0}'.format(response.json()['origin']))
 
-dict = {'a':1,'b':2,'c':3,'d':4}
+#dict = {'a':1,'b':2,'c':3,'d':4}
 #print(dict)
 #print(dict['a'])
 #print(dict['f'])
@@ -18,19 +19,18 @@ dict = {'a':1,'b':2,'c':3,'d':4}
 ##	print(d['list'])
 #d.setdefault('list', []).append(2)
 #print(d['list'])
-d = {'a': 61, 'b': 62, 'c': 63, 'd': 64, 'e': 65}
-for(key,value) in d.items():
+#d = {'a': 61, 'b': 62, 'c': 63, 'd': 64, 'e': 65}
+#for(key,value) in d.items():
 #	print(key,value)
-
-
 
 #字典的魔术方法
 class MyDic(dict):
-	def __init__(self):
+	def __missing__(self,key):
 		if key.islower():
 			raise keyError(key)
 		else:
-			return self[key.lower()]
+			return self[key.lower()]			
 d = MyDic({'a':61})
+'A' in d
 print(d['A'])
 
